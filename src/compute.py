5bf322aca339046d18c733b7b1c6ff62c3291d86
Stéphane LALLEMAGNE <s.lallemagne-ext@cultura.fr>
2022-09-26 15:41:01 +0200

    init dept list

--- src/compute.py
+++ src/compute.py
@@ -15,7 +15,12 @@ for perm in perms:
     uperms.add("".join(perm))
 print("Du fait de chiffres apparaissant plusieurs fois, il n'y a que {} combinaisons uniques possibles".format(len(uperms)))
 
+ulperms = list(uperms)
+ulperms.sort()
 
-for uperm in uperms:
-    print("{}, {}".format(uperm, int(int(uperm)/3)))
+print("num, permutation, resultat, departements, adjacents, adjacent2a2, tripoint")
+iter = 0
+for uperm in ulperms:
+    iter += 1
+    print("{},{},{},?,?,?,?".format(iter, uperm, int(int(uperm)/3)))
 
